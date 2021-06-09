66a655a087cafc38d5eecc293c3564174e9ba9b2
Nagaprasadvr@#£ <nagaprasadvr246@gmail.com>
2021-06-09 13:38:33 +0530

    new file

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 def plot_it(name):
 
-    from plot2 import randomplot2
+    from updated_probaility_density import randomplot2
     randomplot2()  # Press Ctrl+F8 to toggle the breakpoint.
 
 if __name__ == '__main__':
